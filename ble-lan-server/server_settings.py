0fd5f3a16bf48c10ed14649aa927894f348cd7e0
Guillermo Jimenez Prieto <Geon@air-de-compidragones.local>
2023-03-10 20:59:48 +0100

    Fix deployment

--- ble-lan-server/server_settings.py
+++ ble-lan-server/server_settings.py
@@ -3,4 +3,4 @@ import random
 import string
 
 SECRET_KEY = ''.join(random.choices(string.ascii_lowercase, k=50)) if "SECRET_KEY" not in os.environ.keys() else os.environ['SECRET_KEY']
-SERVICE_PORT = 5000 if "PORT" not in os.environ.keys() else os.environ['PORT']
+SERVICE_PORT = 80 if "PORT" not in os.environ.keys() else os.environ['PORT']
