8f69098e3939509a052f9c16b2359fa3d4f4abd4
Guillermo Jimenez Prieto <guillermo.jimenezprieto@altran.com>
2023-03-14 18:12:29 +0100

    Try to fix database

--- ble_lan_server/ble_lan_server/views.py
+++ ble_lan_server/ble_lan_server/views.py
@@ -1,4 +1,8 @@
-from flask import send_from_directory, render_template, Blueprint
+from flask import send_from_directory, render_template, Blueprint, make_response, current_app
+from ble_lan_server.api.decorators import admin_required
+import json
+
+
 
 import os
 
@@ -14,3 +18,10 @@ def welcome():
 def favicon():
     return send_from_directory(os.path.join("ble_lan_server/static", "favicon.ico"),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@admin_required
+@views.route('/config')
+def config():
+    c = current_app.config
+    return make_response(str(current_app.config))
